4851bbc5315ffb37b52b27a988be4a5576f1e700
Vladimir Lazarev <11465238+wolodyx@users.noreply.github.com>
2026-03-01 16:11:48 +0300

    Инструкции по настройке ubuntu-24.04 для сборки проекта

--- build/setup-linux.sh
+++ build/setup-linux.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 set -e
 
+sudo apt -y install      \
+    git                  \
+    vim                  \
+    cmake                \
+    build-essential      \
+    flex                 \
+    bison                \
+    curl                 \
+    zip                  \
+    unzip                \
+    tar                  \
+    file                 \
+    pkg-config           \
+    autoconf             \
+    autoconf-archive     \
+    automake             \
+    libtool              \
+    python3-venv         \
+    python3-pip          \
+    wayland-protocols    \
+    libwayland-dev       \
+    libwayland-client0   \
+    libxtst-dev          \
+    libxinerama-dev      \
+    libxcursor-dev       \
+    '^libxcb.*-dev'      \
+    libx11-xcb-dev       \
+    libgl1-mesa-dev      \
+    libxrender-dev       \
+    libxi-dev            \
+    libxkbcommon-dev     \
+    libxkbcommon-x11-dev \
+    libsystemd-dev       \
+    libarchive-dev       \
+    libxrandr-dev
+
 projectDir="$PWD"
 if [ ! -d "$projectDir/.git" ]; then
   echo "ERROR! Run this script from the project root: build/$(basename $0)"
@@ -17,10 +53,15 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.sh -disableMetrics
 mv vcpkg ~/bin/vcpkg
+export PATH=$PATH:$HOME/bin
 export VCPKG_ROOT=$HOME/vcpkg
 
 # Install external dependencies
-vcpkg install gtest nlohmann-json
-vcpkg install vulkan-sdk-components glslang[tools]
+vcpkg install gtest nlohmann-json boost-log
+vcpkg install vulkan-sdk-components glslang[tools] vulkan-loader[xcb]
 vcpkg install opencascade
 vcpkg install qt5-base[vulkan]
+
+rm -rf \
+    $VCPKG_ROOT/buildtrees \
+    $VCPKG_ROOT/downloads

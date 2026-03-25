f1549aa0ec714b44ac9e13056b6ca2e1ab13e918
Владимир Лазарев <vlazarev@systematica-consulting.ru>
2026-03-25 11:01:39 +0300

    build: сборка под windows при отсутствующем opencascade

--- cmake/QtUtils.cmake
+++ cmake/QtUtils.cmake
@@ -17,3 +17,9 @@ function(copy_qt_plugin PLUGIN_TARGET DEST_SUBDIR)
     DESTINATION ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/plugins/${DEST_SUBDIR}
   )
 endfunction()
+
+function(get_qt_binary_dir qt_binary_dir)
+  get_target_property(qt_core_location Qt${NUMGEOM_QT_MAJOR_VERSION}::Core LOCATION)
+  get_filename_component(dir ${qt_core_location} DIRECTORY)
+  set(${qt_binary_dir} ${dir} PARENT_SCOPE)
+endfunction()
